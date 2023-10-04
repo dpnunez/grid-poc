ed8b6e272711ad88b17d3a1096379d1adb3252fa
Daniel Pôrto Núñez <dpnunez@Daniels-MacBook-Pro.local>
2023-10-04 17:57:48 -0300

    with chakra

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,35 @@ import { useState } from 'react'
 // import './App.css'
 
 import { motion } from 'framer-motion'
+import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react'
 function App() {
   const [open, setOpen] = useState(false)
 
 	const toggleOpen = () => setOpen(e => !e)
 
   return (
-    <div className='w-screen h-screen flex'>
-			<motion.div className='h-screen w-4 bg-slate-500' animate={{
-				width: open ? '200px' : '4rem',
-				transition: {
-					duration: 0.5,
-					ease: 'easeInOut'
-				}
-			}}>	
-				<button type='button' onClick={toggleOpen}>handle</button>
-			</motion.div>
-			<main className='flex-1 grid grid-cols-3 gap-10 p-3'>
-				{Array.from({ length: 10 }).map((_, i) => (
-					<Card key={i} />
-				))
-				}
-			</main>
-		</div>
+		<ChakraProvider>
+
+			<div className='w-screen h-screen flex'>
+				<motion.div className='h-screen w-4 bg-slate-500' animate={{
+					width: open ? '200px' : '4rem',
+					transition: {
+						duration: 0.5,
+						ease: 'easeInOut'
+					}
+				}}>	
+					<button type='button' onClick={toggleOpen}>handle</button>
+				</motion.div>
+				<Grid templateColumns='repeat(3, 1fr)' gap={6} className='flex-1 p-3 max-h-screen overflow-auto'>
+					{Array.from({ length: 10 }).map((_, i) => (
+						<GridItem key={i}>
+							<Card  />
+						</GridItem>
+						))
+					}
+				</Grid>
+			</div>
+		</ChakraProvider>
   )
 }
 
